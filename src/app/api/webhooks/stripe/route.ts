69c007e58531549395014fb2824d75821c19198f
exclusive lex <justinbecerra98@gmail.com>
2025-05-10 22:07:37 -0700

    Fix Stripe API version type error in webhook route

--- src/app/api/webhooks/stripe/route.ts
+++ src/app/api/webhooks/stripe/route.ts
@@ -3,9 +3,7 @@ import { headers } from 'next/headers'
 import Stripe from 'stripe'
 import prisma from '@/lib/prisma'
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2023-10-16'
-})
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
 
 const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET!
 
