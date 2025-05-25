8b8a0f4877f66b3256041f6924e5aaf8a967234b
exclusive lex <justinbecerra98@gmail.com>
2025-05-25 15:43:11 -0700

    Add fixed background layer to guarantee Khaki Moss background on all pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,10 +6,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>
-        <SessionProviderWrapper>
-          <Header />
-          {children}
-        </SessionProviderWrapper>
+        <div style={{ background: '#7C745', position: 'fixed', inset: 0, zIndex: 0, width: '100vw', height: '100vh' }} />
+        <div style={{ position: 'relative', zIndex: 1 }}>
+          <SessionProviderWrapper>
+            <Header />
+            {children}
+          </SessionProviderWrapper>
+        </div>
       </body>
     </html>
   );
